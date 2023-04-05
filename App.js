09a7b4b10aee70b96658818515a2677e86a088c6
Muddasar Hussain <mudassarmanzoor03@gmail.com>
2023-04-05 21:00:05 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ScrollView, Button } from 'react-native';
+import ListScreen from './src/screen/ListScreen';
+import HomeScreen from './src/screen/HomeScreen';
+import Buttonclick from './src/screen/Buttonclick';
+import ImageReuse from './src/screen/ImageReuse';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
+const btnAction = () => {
+  return ListScreen;
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Buttonclick'>
+
+          <Stack.Screen name="button" component={Buttonclick}
+            screenOptions={{
+              title: "Button Screen",
+              headerStyle: {
+                backgroundColor: 'blue',
+              },
+              headerTintColor: 'white',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                fontSize: 25
+              }
+            }} />
+          <Stack.Screen name="HomeScreen" component={HomeScreen}
+            options={{
+              // Adding header title here
+              // headerTitle: () => <Button onPress={btnAction} title="List" />,
+              // Adding something on header right side.
+              //   headerRight: () => <ImageReuse />,
+              headerStyle: {
+                backgroundColor: 'blue',
+              },
+              headerTintColor: 'white',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                fontSize: 25
+              }
+            }} />
+          <Stack.Screen name="ListScreen" component={ListScreen} />
+          <Stack.Screen name="ImageReuse" component={ImageReuse} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+    </ScrollView>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// {/* Attributes of styling in screen elements if we want to style all screens same 
+//          then we have to use screenOptions attribute otherwise use option attribute.
+//          In my Case, i am using individual styling for homescreen using option attribute and overall app
+//          screen styling for my app in buttonclick component using screenOptions*/}
