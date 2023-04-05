9e5822dda28a14db5178b1a15761cc9098b61a27
Muddasar Hussain <mudassarmanzoor03@gmail.com>
2023-04-05 20:57:04 +0500

    Update Buttonclick.js

--- src/screen/Buttonclick.js
+++ src/screen/Buttonclick.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import { Text, View, Button, StyleSheet, TouchableOpacity } from 'react-native';
-
-const Buttonclick = () => {
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+// const Tab = createBottomTabNavigator();
+// const Drawer = createDrawerNavigator();
+const Tab = createMaterialTopTabNavigator();
+const Buttonclick = ({ navigation }) => {
+    const name = "Muddasar";
+    const age = "20";
     return (
         <View>
             <Button
-                title="Click on me "
-                onPress={() => { console.log("Button pressed") }} />
-            <TouchableOpacity onPress={() => { console.log("Touchable work") }} style={styles.opacity}>
-                <Text>Click on me</Text>
+                title="List Screen"
+                // Passing data from buttonclick to listScreen.
+                onPress={() => navigation.navigate("ListScreen", { name: name, age: age })} />
+            <TouchableOpacity onPress={() => navigation.navigate("HomeScreen")} style={styles.opacity}>
+                <Text>Home Screen</Text>
             </TouchableOpacity>
+            <Tab.Navigator>
+                <Tab.Screen name="Login" component={Login} />
+                <Tab.Screen name="Signup" component={Signup} />
+            </Tab.Navigator>
+            {/* <Drawer.Navigator>
+                <Drawer.Screen name="Login" component={Login} />
+                <Drawer.Screen name="Signup" component={Signup} />
+            </Drawer.Navigator> */}
         </View>
+
     )
 };
-
+const Login = () => {
+    return (<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}><Text>I am Login</Text></View>)
+};
+const Signup = () => {
+    return (<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>I am Signup</Text>
+    </View>)
+};
 const styles = StyleSheet.create({
     opacity: {
         backgroundColor: "red",
